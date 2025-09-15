f682cca118e85e764ebed0e1decdca09f2dba349
NwwVall <vidthekingrblx@gmail.com>
2025-09-15 20:09:43 +0700

    page3

--- page/bagiResep.php
+++ page/bagiResep.php
@@ -1,20 +1,70 @@
-<div class="ft">
-        <a>+</a>
-    </div>
 
-    <div class="gallery">
-        <p>
-        <textarea name="comment" rows="5" cols="40">1.<br>2.<br>3.<br>4.<br>5.</textarea>
-        </p>
+        <!-- Header Section -->
+         <div class="row" style="margin-top: 1rem;">
+            <div class="col-4">
+                <div class="sample-image" >
+                <span>sampul</span>
+            </div>
+            </div>
+
+            <div class="col-8">
+                 <div class="title-input">
+                <input type="text" placeholder="Tuliskan Deskripsi Makanan/Minuman Anda" class="title-field">
+            </div>
+            </div>
+         </div>
+        
+        <!-- Image Grid Section -->
+         <div class="row" style="margin-top: 1rem;">
+            <div class="col">
+                <div class="image-item">
+                    <div class="image-placeholder">
+                        <div class="plus-icon">+</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col"> 
+                <button class="description-btn">Masukan Deskripsi langkah-langkah...</button> 
+            </div>
+
+            <div class="col">
+                <div class="image-item">
+                    <div class="image-placeholder">
+                        <div class="plus-icon">+</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col"> 
+                <button class="description-btn">Masukan Deskripsi langkah-langkah...</button> 
+            </div>
+         </div>
+
+         <div class="row" style="margin-top: 1rem;">
+            <div class="col">
+                <div class="image-item">
+                    <div class="image-placeholder">
+                        <div class="plus-icon">+</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col"> 
+                <button class="description-btn">Masukan Deskripsi langkah-langkah...</button> 
+            </div>
+
+            <div class="col">
+                <div class="image-item">
+                    <div class="image-placeholder">
+                        <div class="plus-icon">+</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col"> 
+                <button class="description-btn">Masukan Deskripsi langkah-langkah...</button> 
+            </div>
+         </div>
+
+        <!-- Share Recipe Button -->
+        <div class="share-section" style="margin-top: 1rem;">
+            <button class="share-btn2">Mulai bagikan resep anda sekarang??</button>
+        </div>
     
-<div class="gallery-item">
-    <a target="_blank" href="img_5terre.jpg"> 
-        <!-- <img src="img.home/Rectangle 4.png" alt="Cinque Terre" width="600" height="400"> -->
-    </a>
-</div>
-    <div class="gallery-item">
-    <a target="_blank" href="img_forest.jpg">
-        <img src="img.home/image 6.png" alt="Forest" width="50" height="50">
-    </a>
-    </div>
-    </div>
